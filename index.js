71e4c7f04a5176b5a269e2f255353230fb5ad071
anikatasnin <jarintasnin09@gmail.com>
2025-03-26 23:19:19 +0600

    set route to post

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://DB_USERNAME:<DB_PASSWORD>@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4vcsd99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,6 +23,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new user: ", user);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +36,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
