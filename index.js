b7c61f6fc37445839d6a546bfcea7301029763a8
anikatasnin <jarintasnin09@gmail.com>
2025-03-28 12:00:24 +0600

    create db and collection

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log("new user", user);
-      const result = await haiku.insertOne(user);
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
